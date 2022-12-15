0a0f5049e09c7825f86c278b0e5fee144c98455f
Leslie Albrecht <rooirampokker@gmail.com>
2022-12-15 11:29:05 +0000

    udpated env settings and variables

--- .circleci/scripts/deploy.sh
+++ .circleci/scripts/deploy.sh
@@ -7,15 +7,15 @@ fi
 
 echo "compressing installation for deployment..."
 cd ~/project
-tar zcf - ./* | ssh -o StrictHostKeyChecking=no ${STAGING_SSH_USERNAME}@${STAGING_SSH_URL} "cat > project.tar.gz"
+tar zcf - ~/project | ssh -o StrictHostKeyChecking=no ${PROD_SSH_USERNAME}@${PROD_SSH_URL} "cat > project.tar.gz"
 
 echo "untar compressed project..."
-ssh -o StrictHostKeyChecking=no ${STAGING_SSH_USERNAME}@${STAGING_SSH_URL} << EOF
-mv project.tar.gz ${STAGING_WEB_ROOT}
-cd ${STAGING_WEB_ROOT}
+ssh -o StrictHostKeyChecking=no ${SPROD_SSH_USERNAME}@${STAGING_SSH_URL} << EOF
+mv project.tar.gz ${PROD_WEB_ROOT}
+cd ${PROD_WEB_ROOT}
 tar -xzf project.tar.gz
-#rm project.tar.gz
-#cp .env.example .env
-#php artisan migrate
+cp .env.production .env
+php artisan key:generate
+php artisan migrate
 
 EOF
