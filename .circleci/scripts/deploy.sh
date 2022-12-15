84e4d3aa1cd66deac85e53197cb55c03d2317aa9
Leslie Albrecht <rooirampokker@gmail.com>
2022-12-15 14:04:56 +0000

    updated tar commands to ensure dot files are included

--- .circleci/scripts/deploy.sh
+++ .circleci/scripts/deploy.sh
@@ -6,16 +6,15 @@ if [[ -z "$CIRCLECI" ]]; then
 fi
 
 echo "compressing installation for deployment..."
-cd ~/project
-tar zcf - ~/project | ssh -o StrictHostKeyChecking=no ${PROD_SSH_USERNAME}@${PROD_SSH_URL} "cat > project.tar.gz"
+tar zcf - project | ssh -o StrictHostKeyChecking=no ${PROD_SSH_USERNAME}@${PROD_SSH_URL} "cat > project.tar.gz"
 
 echo "untar compressed project..."
-ssh -o StrictHostKeyChecking=no ${SPROD_SSH_USERNAME}@${STAGING_SSH_URL} << EOF
+ssh -o StrictHostKeyChecking=no albrecht.uk.com@ssh.gb.stackcp.com << EOF
 mv project.tar.gz ${PROD_WEB_ROOT}
 cd ${PROD_WEB_ROOT}
-tar -xzf project.tar.gz
-cp .env.production .env
-php artisan key:generate
+tar -xzf project.tar.gz --strip-components=1 -C ./
+rm project.tar.gz
+mv .env.production .env
 php artisan migrate
 
 EOF
