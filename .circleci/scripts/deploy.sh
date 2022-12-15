bf09957e3eec4b35e6d2759bfb1bf1289a868fe2
Leslie Albrecht <rooirampokker@gmail.com>
2022-12-15 21:30:10 +0000

    update path to project folder again

--- .circleci/scripts/deploy.sh
+++ .circleci/scripts/deploy.sh
@@ -6,13 +6,13 @@ if [[ -z "$CIRCLECI" ]]; then
 fi
 
 echo "compressing installation for deployment..."
-tar zcf - ./project | ssh -o StrictHostKeyChecking=no ${PROD_SSH_USERNAME}@${PROD_SSH_URL} "cat > project.tar.gz" 
+tar zcf - ~/project | ssh -o StrictHostKeyChecking=no ${PROD_SSH_USERNAME}@${PROD_SSH_URL} "cat > project.tar.gz"
 
 echo "untar compressed project..."
 ssh -o StrictHostKeyChecking=no albrecht.uk.com@ssh.gb.stackcp.com << EOF
 mv project.tar.gz ${PROD_WEB_ROOT}
 cd ${PROD_WEB_ROOT}
-tar -xzf project.tar.gz --strip-components=1 -C ./
+tar -xzf project.tar.gz --strip-components=3 -C ./
 rm project.tar.gz
 mv .env.production .env
 php artisan migrate
