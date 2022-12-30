eb5463de79b4abb02684653ff880308af391d89e
Leslie Albrecht <rooirampokker@gmail.com>
2022-12-30 23:00:37 +0000

    echo env variables to test

--- .circleci/scripts/deploy.sh
+++ .circleci/scripts/deploy.sh
@@ -15,12 +15,13 @@ cd ${PROD_WEB_ROOT}
 tar -xzf project.tar.gz --strip-components=3 -C ./
 rm project.tar.gz
 
-echo "update .env with variables"
+echo "update .env with variables..."
 mv .env.production .env
-export PROD_DB_USER=${PROD_DB_USER}
-export PROD_DB_PASSWORD=${PROD_DB_PASSWORD}
-export PROD_DB_HOST=${PROD_DB_HOST}
-export PROD_DB_SCHEMA=${PROD_DB_SCHEMA}
+
+export PROD_DB_USER=${PROD_DB_USER} | echo $PROD_DB_USER
+export PROD_DB_PASSWORD=${PROD_DB_PASSWORD} | echo $PROD_DB_PASSWORD
+export PROD_DB_HOST=${PROD_DB_HOST} | echo $PROD_DB_HOST
+export PROD_DB_SCHEMA=${PROD_DB_SCHEMA} | echo $PROD_DB_SCHEMA
 
 php artisan key:generate
 php artisan migrate
