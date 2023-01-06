2e36af0cb4f804b31512f69440d2430f80a7676c
Leslie Albrecht <rooirampokker@gmail.com>
2023-01-06 13:46:20 +0000

    linter updates

--- app/Notifications/PasswordResetRequest.php
+++ app/Notifications/PasswordResetRequest.php
@@ -8,7 +8,6 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-
 class PasswordResetRequest extends Notification implements ShouldQueue
 {
     use Queueable;
