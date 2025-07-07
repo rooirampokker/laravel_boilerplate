c2ba8a05625fe3886402f4a2859a89e0db654fbe
leslie-eventogy <leslie@eventogy.com>
2025-07-07 09:32:54 +0100

    restore incorrect deletions

---
+++ app/Http/Requests/RoleUpdateRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class RoleUpdateRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name'      => 'sometimes|required|unique:roles',
+            'guard_name' => 'sometimes|required'
+        ];
+    }
+
+    /**
+     * @param Validator $validator
+     * @return void
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json($validator->errors(), 422));
+    }
+}
