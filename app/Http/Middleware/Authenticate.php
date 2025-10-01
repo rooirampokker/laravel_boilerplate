eb14a69e9ee00a932eb33aac5e6a23c290bf4687
rooirampokker <rooirampokker@gmail.com>
2025-10-01 10:34:56 +0100

    remove unused trait

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,12 +3,9 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
-use App\Traits\ResponseTrait;
 
 class Authenticate extends Middleware
 {
-    use ResponseTrait;
-
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
